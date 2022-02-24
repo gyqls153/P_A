00ce4365aa5c54acaa36dd2dcaee11e03617f0a3
황지영 <ghkd6385@gmail.com>
2022-02-24 22:10:02 +0900

    Subpage-header-navi

---
+++ asomein/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+
+// navigation - 서브페이지 및 해더바로 아래 메뉴 /
+function Navimenu(){
+    return(
+        <div className="header__navi__wrap">
+            <nav className="nav-bbs-tab tabs">
+                 <ul>
+                    <li className="">공지사항/새소식</li>
+                    <li className="">경제뉴스</li>
+                    <li className="">어썸인 YOU 퀴즈?</li>
+                    <li className="">스크랩 뉴스</li>
+                    <li className="">재테크수다방</li>
+                    <li className="">이용 후기</li>
+                    <li className="">어썸인에게 문의하기</li>
+                 </ul>
+             </nav>
+        </div>
+    )
+}
+export default Navimenu;
