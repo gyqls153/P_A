1d3904b39f17df61d5d75132e60a1a3a9fd5511a
황지영 <ghkd6385@gmail.com>
2022-02-25 11:48:24 +0900

    commit

--- asomein/src/components/InfoNavigation.jsx
+++ asomein/src/components/InfoNavigation.jsx
@@ -1,18 +1,20 @@
+import { Link } from 'react-router-dom';
+
 // user info header
 function userinfo(){
     return(
         <div className="header__navi__wrap">
             <nav className="sub-info-nav">
                  <ul>
-                    <li>나의 정보</li>
-                    <li>비밀번호변경</li>
-                    <li>나의 구독서비스</li>
-                    <li>나의 장바구니</li>
-                    <li>나의 결제</li>
-                    <li>나의 작성글</li>
-                    <li>나의 문의글</li>
-                    <li>나의 포인트</li>
-                    <li>설정</li>
+                    <li><Link to="/">나의 정보</Link></li>
+                    <li><Link to="/">비밀번호변경</Link></li>
+                    <li><Link to="/">나의 구독서비스</Link></li>
+                    <li><Link to="/">나의 장바구니</Link></li>
+                    <li><Link to="/">나의 결제</Link></li>
+                    <li><Link to="/">나의 작성글</Link></li>
+                    <li><Link to="/">나의 문의글</Link></li>
+                    <li><Link to="/">나의 포인트</Link></li>
+                    <li><Link to="/">설정</Link></li>
                  </ul>
              </nav>
         </div>
