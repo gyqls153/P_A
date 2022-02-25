839b6b9df95e5ee77422f252a7d4e97ac380ebdf
황지영 <ghkd6385@gmail.com>
2022-02-25 11:30:56 +0900

    commit

--- asomein/src/components/InfoNavigation.jsx
+++ asomein/src/components/InfoNavigation.jsx
@@ -1,7 +1,21 @@
-
+// user info header
 function userinfo(){
     return(
-        <></>
+        <div class="header__navi__wrap">
+            <nav class="sub-info-nav">
+                 <ul>
+                    <li>나의 정보</li>
+                    <li>비밀번호변경</li>
+                    <li>나의 구독서비스</li>
+                    <li>나의 장바구니</li>
+                    <li>나의 결제</li>
+                    <li>나의 작성글</li>
+                    <li>나의 문의글</li>
+                    <li>나의 포인트</li>
+                    <li>설정</li>
+                 </ul>
+             </nav>
+        </div>
     )
 }
 export default userinfo;
