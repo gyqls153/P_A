0119e09fb4938db97196f819d87e5548f42a3004
황지영 <ghkd6385@gmail.com>
2022-03-01 12:47:39 +0900

    commit

--- asomein/src/components/LoginSns.jsx
+++ asomein/src/components/LoginSns.jsx
@@ -1,16 +1,17 @@
+import { Link } from 'react-router-dom';
 
 function LoginSns(props)
 {
     return(
         <div className="login-sns">
-            <a href="!#" className="naver_sns_btn link_naver">
+            <Link to="/" className="naver_sns_btn link_naver">
                 <div className="thumb_sns"></div>
                 <span>네이버로 로그인</span>
-            </a>
-            <a href="!#" className="kakao_sns_btn link_kakao">
+            </Link>
+            <Link to="/" className="kakao_sns_btn link_kakao">
                 <div className="thumb_sns"></div>
                 <span>카카오로 로그인</span>
-            </a>
+            </Link>
         </div>
     )
 }
