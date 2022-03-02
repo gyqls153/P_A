a37bae831f68c624225f84e0634d7413a3588e38
황지영 <ghkd6385@gmail.com>
2022-03-02 18:18:01 +0900

    modal

--- asomein/src/services/Modal.js
+++ asomein/src/services/Modal.js
@@ -12,18 +12,29 @@ const Modal = (props) => {
       {open ? (
         <section className="Main-contents-section">
 
-          <header> {/*  */}
+          <header> 
             {header}
             <button className="close" onClick={close}>
               &times;
             </button>
           </header>
 
-          <main>{props.children}</main>
+          <main>
+            {props.children}
             <div className="sec-modal-unknow">
-              
-              
+                <p>회원가입/로그인 안내</p>
+                <p>해당서비스는 회원 전용 서비스입니다.<br/> 
+                    비회원인 경우 회원가입을 통해<br/>
+                    로그인하시면 됩니다.
+                </p>
+                <p>
+                지금 가입하시면 해당 뉴스레터<br/>
+                구독서비스를 안내 받으실 수 있습니다.
+                </p>
             </div>
+          </main>
+            
+
           <footer className="closing-page-btn">
             <button className="close" onClick={close}>
               회원가입
